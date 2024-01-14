93ba60789a0356572802b54f52928c88aae21b4b
arvindkurmi123 <arvindkurmisirvaiya@gmail.com>
2024-01-14 18:01:38 +0530

    cafeOwner schema added

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const cafeOwner = require("../models/cafeOwner");
 
 module.exports.renderSignupForm = (req,res)=>{
     res.render("users/signup.ejs");
@@ -6,17 +7,19 @@ module.exports.renderSignupForm = (req,res)=>{
 
 module.exports.registerUser = async(req,res)=>{
     try{
-        let {username, email, password} = req.body;
-    let newUser = new User({email,username});
-    let registeredUser = await User.register(newUser,password);
-    console.log(registeredUser);
-    req.login(registeredUser,(err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","Welcome to wanderlust");
-        res.redirect("/listings");
-    })
+        let {username, email, password, UT} = req.body;
+        let newUser = new User({email,UT,username});
+        let registeredUser = await User.register(newUser,password);
+        console.log(registeredUser);
+        let newCafeOwner = new cafeOwner({email});
+        await newCafeOwner.save();
+        req.login(registeredUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","Welcome to wanderlust");
+            res.redirect("/listings");
+        })
     } catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
